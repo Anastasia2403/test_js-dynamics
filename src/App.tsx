b61f9c873154ae04598c300f8e234ae6cce43314
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-07-02 10:49:05 +0300

    created header & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
-        <Header />
+      <Header />
+      <Outlet />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,27 @@
-import { createRoot } from 'react-dom/client';
+import ReactDom from 'react-dom';
+import {
+  HashRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+ReactDom.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/help" element={<h1>Help</h1>} />
+        <Route path="/chat" element={<h1>Chat</h1>} />
+        <Route path="/sportsbook" element={<h1>Sports Book</h1>} />
+        <Route path="/e-sports" element={<h1>E-Sports</h1>} />
+        <Route path="/virtual-sports" element={<h1>Virtual Sports</h1>} />
+        <Route path="/4d" element={<h1>4D</h1>} />
+        <Route path="/casino" element={<h1>Casino</h1>} />
+        <Route path="games" element={<h1>Games</h1>} />
+        <Route path="keno" element={<h1>Promotions</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>,
+  document.getElementById('root')
+);
