fc91163dd52a350f5d1ff56c82d2c99e750c2e4f
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-07-07 15:46:57 +0300

    started sidebar, added change to gitignor

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,13 +1,24 @@
 import style from "./sideBar.module.css";
+import redo from "../../icons/redo.png";
+import arrow from "../../icons/arrowhead-up.png";
+import classNames from "classnames";
 
 export const SideBar: React.FC = () => {
   return (
     <div className={style.sideBar}>
-      <div className={style.firstBlock}>
-        <div className={style.firstBlockItem}>
-          <p className={style.firstBlockItemText}>e06903003005</p>
-          {/* <img src="" alt="" /> */}
+      <div className={style.topBlock}>
+        <div className={style.topBlockItem}>
+            <p>e06903003005</p>
+            <button className={style.refreshButton}>
+                <img src={redo} alt="redo" />
+            </button>
         </div>
+          <div className={style.topBlockItem}>
+              <p className={style.blueText}>Config Now</p>
+              <button className={classNames( style.configButton)}>
+                  <img src={arrow} alt="arrow" />
+              </button>
+          </div>
       </div>
     </div>
   );
