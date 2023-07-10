8546ad3884565d165d87d89977bfc8c6e8a90ee6
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-07-10 14:10:51 +0300

    finished all blocks of sidebar

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,10 +1,15 @@
 import classNames from "classnames";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import style from "./sideBar.module.css";
 import redo from "../../icons/redo.png";
 import arrow from "../../icons/arrowhead-up.png";
+import { useState } from "react";
 
 export const SideBar: React.FC = () => {
+  const [inputValue, setInputValue] = useState(10);
+  const [isPendingOpen, setIsPendingOpen] = useState(true);
+  const [isLastOpen, setIsLastOpen] = useState(false);
+  const [isBetListOpen, setIsBetListOpen] = useState(false);
   return (
     <div className={style.sideBar}>
       <div className={style.sideBarBlock}>
@@ -18,7 +23,7 @@ export const SideBar: React.FC = () => {
           <Link to="https://www.cmd328.com/member/lists/password.aspx?type=3" className={style.blueText} target="_blank">
             Config Now
           </Link>
-          <button className={classNames(style.configButton)}>
+          <button className={classNames(style.openMore)}>
             <img src={arrow} alt="arrow" />
           </button>
         </div>
@@ -73,9 +78,66 @@ export const SideBar: React.FC = () => {
         <div className={style.coeficientInfoInput}>
           <label>
             EUR:
-            <input type="text" value={10} className={style.inputBlock} />
+            <input type="text" value={inputValue} className={style.inputBlock} onChange={(e) => setInputValue(Number(e.target.value))} />
           </label>
         </div>
+        <div className={classNames(style.coeficientInfoValue, style.coeficientInfoValuePayout)}>
+          <p className={style.coeficientInfoValueTitle}>Max Payout</p>
+          <p className={style.coeficientInfoValueContent}>8.50</p>
+        </div>
+        <div className={style.coeficientInfoValue}>
+          <p className={style.coeficientInfoValueTitle}>Bet Limit</p>
+          <p className={classNames(style.coeficientInfoValueContent, style.contentBlue)}>5 / 750</p>
+        </div>
+        <div className={style.coeficientInfoButton}>
+          <button className={classNames(style.infoButton, style.buttonBet)}>Bet</button>
+          <button className={classNames(style.infoButton, style.buttonCancel)}>Cancel</button>
+        </div>
+      </div>
+
+      <NavLink to="#" className={style.liveCenter}>
+        <p className={style.liveCenterTitle}>Live Center</p>
+        <div className={style.liveCenterCounter}>16</div>
+      </NavLink>
+
+      <NavLink to="#" className={style.favorites}>
+        <p className={style.favoritesTitle}>My Favorites</p>
+      </NavLink>
+
+      <button className={style.betList} onClick={() => setIsBetListOpen(!isBetListOpen)}>
+        <div className={style.betListContainer}>
+          <button className={style.openMore}>
+            <img src={arrow} alt="arrow" />
+          </button>
+          <p className={style.betListTitle}>Mini Bet List</p>
+        </div>
+        <button className={classNames(style.refreshButton, style.refreshButtonForecast)}>
+          <img src={redo} alt="arrow" />
+        </button>
+      </button>
+
+      <div className={classNames(style.betListContent, isBetListOpen && style.betListOpen)}>
+        <div className={style.betListContentMain}>
+          <button
+            className={classNames(style.betListContentItem, isPendingOpen && style.active)}
+            onClick={() => {
+              setIsPendingOpen(true);
+              setIsLastOpen(false);
+            }}
+          >
+            Pending Bets
+          </button>
+          <button
+            className={classNames(style.betListContentItem, isLastOpen && style.active)}
+            onClick={() => {
+              setIsPendingOpen(false);
+              setIsLastOpen(true);
+            }}
+          >
+            Last Bets
+          </button>
+        </div>
+        <p className={style.betListContentBottom}>No information is available</p>
       </div>
     </div>
   );
